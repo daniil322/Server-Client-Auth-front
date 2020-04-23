e379c897e5025d0545baf499d83588c308fe00c4
Daniil Glezer <daniilglezer@Daniils-MacBook-Pro.local>
2020-04-23 10:38:28 +0300

    first commit

---
+++ src/actions/user-action.js
@@ -0,0 +1,20 @@
+import {login} from "../services/user-service";
+
+function setLoggedInUserAction(user) {
+  return {
+    type: 'SET_USER',
+    user
+  };
+}
+
+export function setLoggedInUser(user) {
+  return async (dispatch) => {
+    try {
+      const LoggedInUser = await login(user);
+      dispatch(setLoggedInUserAction(LoggedInUser));
+      return LoggedInUser;
+    } catch (err) {
+      console.log("Wrong Password/Username", err);
+    }
+  };
+}
---
+++ src/services/user-service.js
@@ -0,0 +1,22 @@
+import Axios from 'axios'
+
+const BASE_URL = process.env.NODE_ENV === 'production'
+    ? '/api/user'
+    : '/localhost:3030/api/user'
+
+let axios = Axios.create({
+    withCredentials: true
+});
+
+async function login(user) {
+    const userData = await axios.post(`${BASE_URL}`, user)
+    return userData.data
+}
+
+async function update(user) {
+    const userData = await axios.put(`${BASE_URL}/${id}`, user)
+    return userData.data
+}
+
+export { login, update }
+
